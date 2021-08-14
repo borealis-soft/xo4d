05a4693993905754a9138b9905e5737626de5017
Twixuss <Twixuss@yandex.ru>
2021-08-14 10:52:48 +0300

    part 1

---
+++ Assets/Scripts/CameraMover.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMover : MonoBehaviour
+{
+	[SerializeField] private float noiseSpeed = 0.25f;
+	[SerializeField] private float noiseScale = 0.5f;
+	Vector3 position;
+	private void Awake()
+	{
+		position = transform.position;
+	}
+	void Update()
+    {
+		transform.position = position + new Vector3(
+			noiseScale * (Mathf.PerlinNoise(Time.time * noiseSpeed, 0.5f) - 0.5f),
+			noiseScale * (Mathf.PerlinNoise(Time.time * noiseSpeed + 3.1415f, 0.5f) - 0.5f),
+			0
+		);
+
+	}
+}
---
+++ Assets/Scripts/Cell.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Cell : MonoBehaviour
+{
+	[SerializeField] private GameObject crossPrefab, zeroPrefab;
+	[SerializeField] private GameObject highlight;
+
+	[HideInInspector] public int localX, localY;
+	[HideInInspector] public int fieldX, fieldY;
+
+	private bool hasMoved;
+	private void Awake()
+	{
+		highlight.SetActive(false);
+	}
+	private void OnMouseEnter()
+	{
+		highlight.SetActive(true);
+	}
+	private void OnMouseExit()
+	{
+		highlight.SetActive(false);
+	}
+	private void OnMouseUp()
+	{
+		if (hasMoved)
+			return;
+
+		/*
+		switch (Manager.CurrentPlayer)
+		{
+			case CellState.Cross: Instantiate(crossPrefab, transform.position, Quaternion.identity); break;
+			case CellState.Zero:  Instantiate(zeroPrefab,  transform.position, Quaternion.identity); break;
+			default: Debug.Assert(false); break;
+		}
+		Manager.Instance.MakeMove(x + 1, y + 1);
+		*/
+		switch (Random.Range(0, 2))
+		{
+			case 0: Instantiate(crossPrefab, transform.position, Quaternion.identity); break;
+			case 1: Instantiate(zeroPrefab, transform.position, Quaternion.identity); break;
+			default: Debug.Assert(false); break;
+		}
+		Debug.Log($"Clicked fieldX:{fieldX}, fieldX:{fieldY}, localX:{localX}, localX:{localY}");
+
+		hasMoved = true;
+	}
+}
