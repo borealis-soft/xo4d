4a0c8672c0c45291ae4565728706311ec5ce9543
Twixuss <Twixuss@yandex.ru>
2021-08-14 11:33:21 +0300

    move animation

---
+++ Assets/WriteAnimation.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WriteAnimation : MonoBehaviour
+{
+    Material material;
+    float startTime;
+    void Awake()
+    {
+        startTime = Time.time;
+        material = GetComponent<MeshRenderer>().material;
+        StartCoroutine(Animate());
+    }
+    IEnumerator Animate()
+	{
+        while (true)
+		{
+            float t = Time.time - startTime;
+            material.SetFloat("_TimeOffset", t);
+            if (t >= 1)
+		    {
+                yield break;
+		    }
+            yield return null;
+		}
+    }
+}
